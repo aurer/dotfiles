58edf8944c7bec63fd6a02ad8e49615442c6fb93
Phil <philmau@gmail.com>
2022-02-06 20:15:11 +0000

    Add new files

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Check for zsh
+if [ "$SHELL" != "/bin/zsh" ]; then 
+	echo "It looks like your not using zsh, please install this before continuing"
+	exit 1
+fi
+
+# Set the base directory
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE}" )" && pwd );
+
+# Create required folders
+echo "Create required folders"
+mkdir -p ~/.config/zsh
+mkdir -p ~/.cache/zsh
+
+# Install zsh configuration
+echo "Install zsh configuation"
+cp -R $BASEDIR/files/config/zsh/* ~/.config/zsh/
+cp $BASEDIR/files/zshrc ~/.zshrc
+
+# Install micro configuration
+echo "Install micro configuration"
+mkdir -p ~/.config/micro
+cp $BASEDIR/files/config/micro/* ~/.config/micro/
+
+# Setup vim
+echo "Setup vim"
+cp -r $BASEDIR/files/vim/* ~/.vim/
+
+# Check for git user.name
+echo "Configure git"
+
+# Copy configs
+mkdir -p ~/.config/git
+cp -r $BASEDIR/files/config/git/* ~/.config/git/
+
+# Include config
+if [ ! -f ~/.gitconfig ]; then 
+	printf "[include]\n  path = ~/.config/git/config\n\n" > ~/.gitconfig
+fi
+
+# Set user.name
+if [[ ! $(git config user.name) ]]; then
+	read -p "Git user.name: " name;
+	git config --global user.name "$name";
+fi
+
+# Set user.email
+if [[ ! $(git config user.email) ]]; then
+	read -p "Git user.email: " email;
+	git config --global user.email "$email";
+fi
+
+echo "Done"
+unset BASEDIR
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Dotfiles
+
+## Installation
+
+1. Clone this repo somewhere sensible
+2. In a terminal run `install.sh`
+3. Run `source ~/.zshrc` or `exec $SHELL -l` to reload your configuration
+
+## Extras
+
+* `files/iterm/profile.json` can be imported into iTerm as a profile
+* `files/iterm/aura.itermcolors` can be imported into iTerm as a schema
